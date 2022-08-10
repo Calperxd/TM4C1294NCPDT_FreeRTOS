7ec754a23df475ff2535e238aac4d348546c0b80
Gabriel Alexandre Linhares Calper Seabra <calper@pop-os.localdomain>
2022-08-09 23:52:43 -0300

    build: update of build process

--- Firmware/project2/gcc/startup_gcc.d
+++
@@ -1,9 +0,0 @@
-gcc/startup_gcc.o: startup_gcc.c \
- /home/calper/gcc-arm-11.2-2022.02-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/11.2.1/include/stdint.h \
- /home/calper/gcc-arm-11.2-2022.02-x86_64-arm-none-eabi/arm-none-eabi/include/stdint.h \
- /home/calper/gcc-arm-11.2-2022.02-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
- /home/calper/gcc-arm-11.2-2022.02-x86_64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
- /home/calper/gcc-arm-11.2-2022.02-x86_64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
- /home/calper/gcc-arm-11.2-2022.02-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
- /home/calper/gcc-arm-11.2-2022.02-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
- ../../TivaWare/inc/hw_nvic.h ../../TivaWare/inc/hw_types.h
