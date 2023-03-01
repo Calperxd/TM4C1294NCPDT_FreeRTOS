793bca9e0e84755d01f805fb94fdad2d2c0b0f7d
Gabriel Calper Seabra <gabriel.seabra@itriad.org.br>
2023-03-01 02:19:53 -0300

    feat: added gui code for Acoustic gas analyzer

---
+++ GUI/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.txtTime = new System.Windows.Forms.RichTextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.scrllVolume = new System.Windows.Forms.VScrollBar();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.lblGain = new System.Windows.Forms.Label();
+            this.btnAmostragem = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClose);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbPort);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 216);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(188, 110);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(112, 31);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Desconectar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(28, 110);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(97, 31);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Conectar";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PORTA";
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(188, 40);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(133, 24);
+            this.cbPort.TabIndex = 0;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(614, 46);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(57, 25);
+            this.txtTime.TabIndex = 2;
+            this.txtTime.Text = "15";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(376, 46);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(214, 25);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "Tempo de amostragem";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(574, 98);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(97, 31);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "Baixar CSV";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(534, 241);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(97, 31);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(691, 241);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(97, 31);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // scrllVolume
+            // 
+            this.scrllVolume.Location = new System.Drawing.Point(124, 130);
+            this.scrllVolume.Name = "scrllVolume";
+            this.scrllVolume.Size = new System.Drawing.Size(21, 100);
+            this.scrllVolume.TabIndex = 7;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVolume.Location = new System.Drawing.Point(96, 105);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(79, 25);
+            this.lblVolume.TabIndex = 8;
+            this.lblVolume.Text = "Volume";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnMinus);
+            this.groupBox2.Controls.Add(this.btnPlus);
+            this.groupBox2.Controls.Add(this.lblGain);
+            this.groupBox2.Controls.Add(this.scrllVolume);
+            this.groupBox2.Controls.Add(this.lblVolume);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(445, 184);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 246);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ruído Branco";
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(299, 147);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(38, 33);
+            this.btnMinus.TabIndex = 12;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(255, 147);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(38, 33);
+            this.btnPlus.TabIndex = 11;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // lblGain
+            // 
+            this.lblGain.AutoSize = true;
+            this.lblGain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGain.Location = new System.Drawing.Point(258, 105);
+            this.lblGain.Name = "lblGain";
+            this.lblGain.Size = new System.Drawing.Size(71, 25);
+            this.lblGain.TabIndex = 9;
+            this.lblGain.Text = "Ganho";
+            // 
+            // btnAmostragem
+            // 
+            this.btnAmostragem.Location = new System.Drawing.Point(381, 99);
+            this.btnAmostragem.Name = "btnAmostragem";
+            this.btnAmostragem.Size = new System.Drawing.Size(160, 31);
+            this.btnAmostragem.TabIndex = 10;
+            this.btnAmostragem.Text = "Iniciar Amostragem";
+            this.btnAmostragem.UseVisualStyleBackColor = true;
+            this.btnAmostragem.Click += new System.EventHandler(this.btnAmostragem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 450);
+            this.Controls.Add(this.btnAmostragem);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox txtTime;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.VScrollBar scrllVolume;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAmostragem;
+        private System.Windows.Forms.Label lblGain;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
+    }
+}
+
---
+++ GUI/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Timers;
+using System.Windows.Threading;
+using System.Threading;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        private const string V = "Amostra";
+        private WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+        private string dataIn;
+        List<string> list = new List<string>();
+        private int teste = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            player.settings.volume = scrllVolume.Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cbPort.Items.AddRange(ports);
+            btnClose.Enabled = false;
+            btnDownload.Enabled = false;
+            btnAmostragem.Enabled = false;
+            txtTime.Enabled = false;
+            btnPlus.Enabled = false;
+            btnMinus.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                serialPort1.PortName = cbPort.Text;
+                serialPort1.BaudRate = 921600;
+                serialPort1.DataBits = 8;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Open();
+                if (serialPort1.IsOpen)
+                {
+                    btnClose.Enabled = true;
+                    btnDownload.Enabled = true;
+                    btnOpen.Enabled = false;
+                    btnAmostragem.Enabled = true;
+                    txtTime.Enabled = true;
+                    btnPlus.Enabled = true;
+                    btnMinus.Enabled = true;
+                    cbPort.Enabled = false;
+                }
+                
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                btnClose.Enabled = false;
+                btnDownload.Enabled = false;
+                btnOpen.Enabled = true;
+                btnAmostragem.Enabled = false;
+                txtTime.Enabled = false;
+                btnPlus.Enabled = false;
+                btnMinus.Enabled = false;
+                cbPort.Enabled = true;
+                serialPort1.Close();
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            player.URL = "C:\\Users\\Galc_\\OneDrive\\UTFPR\\TCC\\GUI\\WindowsFormsApp1\\white-noise.mp3";
+            player.controls.play();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            player.controls.stop();
+        }
+
+        private void btnAmostragem_Click(object sender, EventArgs e)
+        {
+            list.Clear();
+            serialPort1.Write("#");
+            int time = Int32.Parse(txtTime.Text);
+            serialPort1.Write(time.ToString("00"));
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("$");
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("%");
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            dataIn = serialPort1.ReadExisting();
+            this.Invoke(new EventHandler(ShowData));
+        }
+
+        private void ShowData(object sender, EventArgs e)
+        {
+            list.Add(dataIn);
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string strWorkPath = System.IO.Path.GetDirectoryName(strExeFilePath);
+            teste++;
+            for (int i = 1; i < list.Count; i++)
+            {
+                File.AppendAllText(strWorkPath + "\\Amostra" + teste.ToString() + ".csv", list.ToArray().ElementAt(i));
+            }
+        }
+    }
+}
